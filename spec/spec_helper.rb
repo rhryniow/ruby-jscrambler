5a2ef43d18608eff0d54384786dd925e7091d649
José P. Airosa <me@joseairosa.com>
2014-09-25 11:29:42 +0100

    Setting up testing framework

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'bundler/setup'
+require 'rspec'
+Bundler.setup
+
+require 'jscrambler'
+
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  config.color_enabled = true
+  config.formatter     = 'documentation'
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+end
