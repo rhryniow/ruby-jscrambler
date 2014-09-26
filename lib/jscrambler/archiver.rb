06b6a2c112056b36a3f9a557cdc54fff606c775a
José P. Airosa <me@joseairosa.com>
2014-09-26 14:51:38 +0100

    Implemented unzip

--- lib/jscrambler/archiver.rb
+++ lib/jscrambler/archiver.rb
@@ -4,23 +4,31 @@ module JScrambler
     attr_accessor :files
     attr_accessor :zipfile
 
-    def initialize(files)
-      @files, @zipfile = files, Tempfile.new(%w(jscrambler .zip))
+    def initialize(files, zipfile=Tempfile.new(%w(jscrambler .zip)))
+      @files, @zipfile = files, zipfile
     end
 
     def zip
-      Zip::File.open(@zipfile.path, Zip::File::CREATE) do |zipfile|
+      Zip::File.open(zipfile.path, Zip::File::CREATE) do |zipfile|
         files.each do |file|
           filename = File.basename(file)
           zipfile.add(filename, file)
         end
         # zipfile.get_output_stream("myFile") { |os| os.write "myFile contains just this" }
       end
-      @zipfile.path
+      zipfile.path
     end
 
-    def unzip
-
+    def unzip(to_path)
+      files = []
+      Zip::File.open(zipfile.path) do |zip_file|
+        zip_file.each do |entry|
+          to_file_path = File.join(to_path, entry.name)
+          files << to_file_path
+          entry.extract(to_file_path)
+        end
+      end
+      files
     end
   end
 end
--- spec/lib/jscrambler/archiver_spec.rb
+++ spec/lib/jscrambler/archiver_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe JScrambler::Archiver do
 
   let(:files) { [Tempfile.new('file1'), Tempfile.new('file2')] }
+  let(:instance) { described_class.new(files.map(&:path)) }
 
   describe '#initialize' do
 
-    subject { described_class.new(files.map(&:path)) }
+    subject { instance }
 
     it 'should create a temporary file' do
       expect(subject.zipfile).to be_a Tempfile
@@ -15,10 +16,27 @@ describe JScrambler::Archiver do
 
   describe '#zip' do
 
-    subject { described_class.new(files.map(&:path)).zip }
+    subject { instance.zip }
 
     it 'should generate a zipfile' do
       expect(File.exists?(subject)).to be true
+    end
+  end
+
+  describe '#unzip' do
+
+    let(:to_path) { Dir.mktmpdir }
+
+    before do
+      instance.zip
+    end
+
+    subject { instance.unzip(to_path) }
+
+    it 'should generate a zipfile' do
+      subject.each do |file|
+        expect(File.exists?(File.join(to_path, File.basename(file)))).to be true
+      end
     end
   end
 end
