231b5cc03ee7d67ee3b2f145dbbdc2aa5d2b2c81
José P. Airosa <me@joseairosa.com>
2014-10-01 12:44:33 +0100

    Mute logging during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
     stub_const('JScrambler::Config::DEFAULT_CONFIG_FILE', 'spec/fixtures/jscrambler_default_config.json')
 
     logger = Logger.new(STDOUT)
-    logger.level = Logger::INFO
+    logger.level = Logger::ERROR
     stub_const('JScrambler::LOGGER', logger)
   end
 end
